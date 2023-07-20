a53d1af4b3792abfaca94b983af2f5a556c608b5
Fabian Ruettgers <fabulon78@gmail.com>
2023-07-20 15:41:25 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍌 Banana", color: "yellow" },
+    { id: 2, name: "🍎 Apple", color: "red" },
+    { id: 3, name: "🍓 Strawberry", color: "red" },
+    { id: 4, name: "🍍 Pineapple", color: "yellow" },
+    { id: 5, name: "🍊 Orange", color: "orange" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
